617f435ec4e14afa00148c1563bcd2bb8c8aa8c2
Omirakov <omirakov96@gmail.com>
2025-07-24 19:55:41 +0300

    hw12 complete

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
+    public static void main(String[] args) {
 
+        Author author1 = new Author("Andrzej", "Sapkowski");
+        Book book1 = new Book("The tower of the swallow", author1, 1997);
+        System.out.println(book1.getBookName() + " " + book1.getAuthorName() + " " + book1.getPublisherYear());
 
-    public static void main (String[] args) {
+        Author author2 = new Author("Roger", "Zelazny");
+        Book book2 = new Book("The guns of Avalon", author2, 1999);
+        System.out.println(book2.getBookName() + " " + book2.getAuthorName() + " " + book2.getPublisherYear());
 
-
-
+        book2.setPublisherYear(1972);
+        System.out.println(book2.getPublisherYear());
+        System.out.println(book2.getBookName() + " " + book2.getAuthorName() + " " + book2.getPublisherYear());
     }
 
 
